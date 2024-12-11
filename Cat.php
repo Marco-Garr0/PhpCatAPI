3f4bb95ee1139ff65e97b9412e9856cdc8f4a4bb
Marco-Garro <mgarro@elemento.cloud>
2024-12-11 08:29:58 +0100

    added cat properties

--- Cat.php
+++ Cat.php
@@ -2,13 +2,31 @@
 class Cat{
 	private int $id = 0;
 	private ?string $name = null;
+	private ?string $image = null;
 	private ?int $age = 0;
+	private ?string $whereWasFound = null;
+	private ?string $whereWasSeen = null;
+	private ?bool $sex = null;
+	private ?string $price = null;
+	private ?string $color = null;
+	private ?int $weight = 0;
+	private ?string $breed = null;
 
-	public function __construct($id, $name, $age){
+
+	public function __construct($id, $name, $age, $image, $whereWasFound, $whereWasSeen, $sex, $price, $color, $weight, $breed)
+	{
 		$this->id = $id;
 		$this->name = $name;
 		$this->age = $age;
-      	}
+		$this->image = $image;
+		$this->whereWasFound = $whereWasFound;
+		$this->whereWasSeen = $whereWasSeen;
+		$this->sex = $sex;
+		$this->price = $price;
+		$this->color = $color;
+		$this->weight = $weight;
+		$this->breed = $breed;
+	}
 
 	public function getId(): int{
 		return $this->id;
@@ -20,6 +38,30 @@ class Cat{
 
 	public function getAge(): ?int{
 		return $this->age;
+	}
+	public function getImage(): ?String{
+		return $this->image;
+	}
+	public function getWhereWasFound(): ?String{
+		return $this->whereWasFound;
+	}
+	public function getWhereWasSeen(): ?String{
+		return $this->whereWasSeen;
+	}
+	public function getSex(): ?bool{
+		return $this->sex;
+	}
+	public function getPrice(): ?String{
+		return $this->price;
+	}
+	public function getColor(): ?String{
+		return $this->color;
+	}
+	public function getWeight(): ?int{
+		return $this->weight;
+	}
+	public function getBreed(): ?String{
+		return $this->breed;
 	}
 
 	public function setId($id): void{
@@ -33,6 +75,31 @@ class Cat{
 	public function setAge(?int $age){
 		$this->age = $age;
 	}
+	public function setImage(?String $image){
+		$this->image = $image;
+	}
+	public function setWhereWasFound(?String $whereWasFound){
+		$this->whereWasFound = $whereWasFound;
+	}
+	public function setWhereWasSeen(?String $whereWasSeen){
+		$this->whereWasSeen = $whereWasSeen;
+	}
+	public function setSex(?bool $sex){
+		return $this->sex = $sex;
+	}
+	public function setPrice(?String $price){
+		$this->price = $price;
+	}
+	public function setColor(?String $color){
+		$this->color = $color;
+	}
+	public function setWeight(?int $weight){
+		$this->weight = $weight;
+	}
+	public function setBreed(?String $breed){
+		$this->breed = $breed;
+	}
+
 
 	public function __toString(): String{
 		return "[cat] id:".$this->id."\nname: ".$this->name."\nage:".$this->age."\n";
@@ -52,9 +119,13 @@ class Cat{
 	}
 	public static function jsonToCat(String $json): ?Cat{
 		$decoded = json_decode($json);
-		if($decoded->id === null || $decoded->name == null || $decoded->age == null)
+		if($decoded->id === null || $decoded->name == null || $decoded->age == null
+			|| $decoded->image == null || $decoded->whereWasFound == null || $decoded->whereWasSeen == null ||
+			$decoded->sex == null || $decoded->price == null || $decoded->color == null || $decoded->weight == null || $decoded->breed == null)
 			return null;
-		return new Cat($decoded->id, $decoded->name, $decoded->age);
+		return new Cat($decoded->id, $decoded->name, $decoded->age,
+			$decoded->image, $decoded->whereWasFound, $decoded->whereWasSeen, $decoded->sex, $decoded->price,
+			$decoded->color, $decoded->weight, $decoded->breed);
 	}
 }
 ?>
